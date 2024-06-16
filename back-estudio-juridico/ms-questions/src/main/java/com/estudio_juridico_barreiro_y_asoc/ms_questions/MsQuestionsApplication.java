0a7b4b79073ef97ea2fee1caec635c6cbdd3f077
JulianaAparicio <juliana_aparicio@outlook.com>
2024-06-16 19:12:05 -0300

    Added unversioned files

---
+++ back-estudio-juridico/ms-questions/src/main/java/com/estudio_juridico_barreiro_y_asoc/ms_questions/MsQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.estudio_juridico_barreiro_y_asoc.ms_questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsQuestionsApplication.class, args);
+	}
+
+}
---
+++ back-estudio-juridico/ms-questions/src/test/java/com/estudio_juridico_barreiro_y_asoc/ms_questions/MsQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudio_juridico_barreiro_y_asoc.ms_questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
