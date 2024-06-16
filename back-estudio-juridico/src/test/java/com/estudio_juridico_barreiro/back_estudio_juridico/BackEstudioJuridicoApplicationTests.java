726cbc8a68c9a9cb8116bf2f4bd4bf6d5f67b488
JulianaAparicio <juliana_aparicio@outlook.com>
2024-06-16 18:20:54 -0300

    Added initial backend

---
+++ back-estudio-juridico/src/test/java/com/estudio_juridico_barreiro/back_estudio_juridico/BackEstudioJuridicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudio_juridico_barreiro.back_estudio_juridico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEstudioJuridicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
