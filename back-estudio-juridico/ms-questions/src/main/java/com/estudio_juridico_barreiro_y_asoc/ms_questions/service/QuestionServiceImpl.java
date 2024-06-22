62ac598e09bd3ebe8b57c8dcdd3c5c926519e21b
JulianaAparicio <juliana_aparicio@outlook.com>
2024-06-22 19:24:52 -0300

    Added exception to method signature and logic to check for null values

--- back-estudio-juridico/ms-questions/src/main/java/com/estudio_juridico_barreiro_y_asoc/ms_questions/service/QuestionServiceImpl.java
+++ back-estudio-juridico/ms-questions/src/main/java/com/estudio_juridico_barreiro_y_asoc/ms_questions/service/QuestionServiceImpl.java
@@ -1,10 +1,11 @@
 package com.estudio_juridico_barreiro_y_asoc.ms_questions.service;
 
+import com.estudio_juridico_barreiro_y_asoc.ms_questions.mails.QuestionMail;
 import com.estudio_juridico_barreiro_y_asoc.ms_questions.model.Question;
 import com.estudio_juridico_barreiro_y_asoc.ms_questions.repository.QuestionRepository;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
@@ -14,8 +15,17 @@ public class QuestionServiceImpl implements QuestionService{
     private QuestionRepository questionRepository;
 
     @Override
-    public Question saveQuestion(Question question) {
-        return questionRepository.save(question);
+    public Question saveQuestion(Question question) throws BadRequestException {
+        if (question.getFullName()==null || question.getEmail()==null || question.getPhoneNumber()==null || question.getMessage()==null){
+            //logger.error("The data entered has null values.");
+            throw new BadRequestException("The question has null values.");
+        } else {
+            //logger.debug("Creating new question...");
+            //logger.debug("Sending email with the new question...");
+            QuestionMail.sendQuestionEmail(question);
+            return questionRepository.save(question);
+            //logger.info("The question was created successfully.");
+        }
     }
 
     @Override
