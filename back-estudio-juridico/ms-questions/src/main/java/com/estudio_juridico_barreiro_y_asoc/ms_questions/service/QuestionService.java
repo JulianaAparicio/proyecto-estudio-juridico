e6492540340a3c20589732cb28e49f082d9195d5
JulianaAparicio <juliana_aparicio@outlook.com>
2024-06-22 19:21:24 -0300

    Added exception to method signature

--- back-estudio-juridico/ms-questions/src/main/java/com/estudio_juridico_barreiro_y_asoc/ms_questions/service/QuestionService.java
+++ back-estudio-juridico/ms-questions/src/main/java/com/estudio_juridico_barreiro_y_asoc/ms_questions/service/QuestionService.java
@@ -1,11 +1,12 @@
 package com.estudio_juridico_barreiro_y_asoc.ms_questions.service;
 
 import com.estudio_juridico_barreiro_y_asoc.ms_questions.model.Question;
+import org.apache.coyote.BadRequestException;
 
 import java.util.List;
 
 public interface QuestionService {
-    public Question saveQuestion(Question question);
+    public Question saveQuestion(Question question) throws BadRequestException;
 
     public List<Question> getAllQuestions();
 
